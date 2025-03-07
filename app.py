de89b215daf536b4fcf6f922746470d17ba99b59
nathaniel-avery <nathaniel.avery@gmail.com>
2025-03-06 21:21:41 -0500

    base app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/')
+def hello_world():
+    """Default route that returns "Hello World"."""
+    return jsonify({"message": "Hello World"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
